c6d0a8de15bb62c0f06ef5b3b9976a0f413f766a
andrewleber <andrew6111@gmail.com>
2021-01-17 00:40:10 -0500

    added colour and fonts

---
+++ colours.js
@@ -0,0 +1,173 @@
+colours =
+[
+    {
+        "hex": "A6093D",
+        "anger": 60,
+        "fear": 10,
+        "joy": 50,
+        "sadness": 0,
+        "analytical": 15,
+        "confident": 60,
+        "tentative": 0
+    },
+    {
+        "hex": "76232F",
+        "anger": 60,
+        "fear": 45,
+        "joy": 30,
+        "sadness": 0,
+        "analytical": 60,
+        "confident": 75,
+        "tentative": 0
+    },
+    {
+        "hex": "C8102E",
+        "anger": 75,
+        "fear": 0,
+        "joy": 75,
+        "sadness": 0,
+        "analytical": 10,
+        "confident": 65,
+        "tentative": 0
+    },
+    {
+        "hex": "E0457B",
+        "anger": 0,
+        "fear": 0,
+        "joy": 85,
+        "sadness": 0,
+        "analytical": 0,
+        "confident": 50,
+        "tentative": 0
+    },
+    {
+        "hex": "F88379",
+        "anger": 0,
+        "fear": 0,
+        "joy": 75,
+        "sadness": 0,
+        "analytical": 0,
+        "confident": 65,
+        "tentative": 0
+    },
+    {
+        "hex": "FF8200",
+        "anger": 0,
+        "fear": 0,
+        "joy": 60,
+        "sadness": 0,
+        "analytical": 30,
+        "confident": 70,
+        "tentative": 0
+    },
+    {
+        "hex": "623B2A",
+        "anger": 10,
+        "fear": 30,
+        "joy": 0,
+        "sadness": 60,
+        "analytical": 65,
+        "confident": 40,
+        "tentative": 50
+    },
+    {
+        "hex": "7A9A01",
+        "anger": 0,
+        "fear": 45,
+        "joy": 55,
+        "sadness": 0,
+        "analytical": 65,
+        "confident": 25,
+        "tentative": 15
+    },
+    {
+        "hex": "154734",
+        "anger": 0,
+        "fear": 60,
+        "joy": 20,
+        "sadness": 55,
+        "analytical": 70,
+        "confident": 60,
+        "tentative": 40
+    },
+    {
+        "hex": "009775",
+        "anger": 0,
+        "fear": 0,
+        "joy": 60,
+        "sadness": 0,
+        "analytical": 70,
+        "confident": 70,
+        "tentative": 0
+    },
+    {
+        "hex": "00677F",
+        "anger": 0,
+        "fear": 25,
+        "joy": 0,
+        "sadness": 70,
+        "analytical": 80,
+        "confident": 50,
+        "tentative": 20
+    },
+    {
+        "hex": "56B7D2",
+        "anger": 0,
+        "fear": 10,
+        "joy": 10,
+        "sadness": 75,
+        "analytical": 35,
+        "confident": 30,
+        "tentative": 60
+    },
+    {
+        "hex": "8094DD",
+        "anger": 0,
+        "fear": 60,
+        "joy": 10,
+        "sadness": 40,
+        "analytical": 60,
+        "confident": 65,
+        "tentative": 35
+    },
+    {
+        "hex": "0072CE",
+        "anger": 0,
+        "fear": 10,
+        "joy": 40,
+        "sadness": 50,
+        "analytical": 45,
+        "confident": 60,
+        "tentative": 15
+    },
+    {
+        "hex": "001A72",
+        "anger": 0,
+        "fear": 35,
+        "joy": 0,
+        "sadness": 45,
+        "analytical": 55,
+        "confident": 70,
+        "tentative": 0
+    },
+    {
+        "hex": "5F259F",
+        "anger": 0,
+        "fear": 25,
+        "joy": 45,
+        "sadness": 0,
+        "analytical": 0,
+        "confident": 40,
+        "tentative": 20
+    },
+    {
+        "hex": "3C1053",
+        "anger": 0,
+        "fear": 30,
+        "joy": 10,
+        "sadness": 55,
+        "analytical": 45,
+        "confident": 10,
+        "tentative": 25
+    }
+]
---
+++ fonts.js
@@ -0,0 +1,233 @@
+fonts =
+[
+  {
+    "font": "synthemesc",
+    "anger": -100,
+    "fear": -100,
+    "joy": 85,
+    "sadness": 0,
+    "analytical": -60,
+    "confident": 35,
+    "tentative": 0
+  },
+  {
+    "font": "rowdy",
+    "anger": 75,
+    "fear": 50,
+    "joy": -100,
+    "sadness": 20,
+    "analytical": -100,
+    "confident": 15,
+    "tentative": -100
+  },
+  {
+    "font": "alta-california",
+    "anger": -100,
+    "fear": -100,
+    "joy": 35,
+    "sadness": -60,
+    "analytical": 85,
+    "confident": 65,
+    "tentative": -100
+  },
+  {
+    "font": "shlop",
+    "anger": 30,
+    "fear": 85,
+    "joy": -100,
+    "sadness": 0,
+    "analytical": -100,
+    "confident": -100,
+    "tentative": -100
+  },
+  {
+    "font": "chauncy-pro",
+    "anger": -100,
+    "fear": -100,
+    "joy": 75,
+    "sadness": 0,
+    "analytical": 15,
+    "confident": 10,
+    "tentative": 0
+  },
+  {
+    "font": "sunflower",
+    "anger": 25,
+    "fear": 70,
+    "joy": -100,
+    "sadness": 80,
+    "analytical": -100,
+    "confident": -100,
+    "tentative": 85
+  },
+  {
+    "font": "uppercut-angle",
+    "anger": 35,
+    "fear": 25,
+    "joy": 20,
+    "sadness": -100,
+    "analytical": 0,
+    "confident": 85,
+    "tentative": -100
+  },
+  {
+    "font": "elektrix",
+    "anger": 15,
+    "fear": 25,
+    "joy": 0,
+    "sadness": 30,
+    "analytical": 10,
+    "confident": 35,
+    "tentative": 0
+  },
+  {
+    "font": "totally-gothic",
+    "anger": 35,
+    "fear": 75,
+    "joy": 5,
+    "sadness": 0,
+    "analytical": 10,
+    "confident": 20,
+    "tentative": -100
+  },
+  {
+    "font": "variex",
+    "anger": 0,
+    "fear": 0,
+    "joy": 75,
+    "sadness": -100,
+    "analytical": 25,
+    "confident": 60,
+    "tentative": 0
+  },
+  {
+    "font": "remedy",
+    "anger": -100,
+    "fear": -100,
+    "joy": 85,
+    "sadness": -100,
+    "analytical": 0,
+    "confident": 25,
+    "tentative": 0
+  },
+  {
+    "font": "kewl-script",
+    "anger": -100,
+    "fear": -100,
+    "joy": 85,
+    "sadness": 0,
+    "analytical": 20,
+    "confident": 50,
+    "tentative": 0
+  },
+  {
+    "font": "ccmonstermashoutline",
+    "anger": 50,
+    "fear": 15,
+    "joy": 0,
+    "sadness": -100,
+    "analytical": 0,
+    "confident": 50,
+    "tentative": 0
+  },
+  {
+    "font": "tangelo",
+    "anger": -100,
+    "fear": -100,
+    "joy": 75,
+    "sadness": -50,
+    "analytical": 0,
+    "confident": 25,
+    "tentative": 0
+  },
+  {
+    "font": "cinque-donne",
+    "anger": -100,
+    "fear": -100,
+    "joy": 20,
+    "sadness": 20,
+    "analytical": 40,
+    "confident": 55,
+    "tentative": 0
+  },
+  {
+    "font": "salvation",
+    "anger": 25,
+    "fear": 30,
+    "joy": 0,
+    "sadness": 60,
+    "analytical": 0,
+    "confident": 0,
+    "tentative": 10
+  },
+  {
+    "font": "tomarik-display",
+    "anger": 0,
+    "fear": 0,
+    "joy": 20,
+    "sadness": 0,
+    "analytical": 75,
+    "confident": 25,
+    "tentative": 10
+  },
+  {
+    "font": "chantal",
+    "anger": 0,
+    "fear": 10,
+    "joy": 0,
+    "sadness": 75,
+    "analytical": 0,
+    "confident": 20,
+    "tentative": 25
+  },
+  {
+    "font": "chaloops",
+    "anger": -100,
+    "fear": -100,
+    "joy": 70,
+    "sadness": 0,
+    "analytical": 40,
+    "confident": 65,
+    "tentative": 15
+  },
+  {
+    "font": "p22-pooper-black-pro",
+    "anger": -100,
+    "fear": 15,
+    "joy": 0,
+    "sadness": 75,
+    "analytical": 0,
+    "confident": 25,
+    "tentative": 35
+  },
+  {
+    "font": "bufalino",
+    "anger": 0,
+    "fear": 35,
+    "joy": 10,
+    "sadness": 45,
+    "analytical": 50,
+    "confident": 10,
+    "tentative": 65
+  },
+  {
+    "font": "input-mono",
+    "anger": 0,
+    "fear": 20,
+    "joy": 0,
+    "sadness": 35,
+    "analytical": 60,
+    "confident": 10,
+    "tentative": 70
+  },
+  {
+    "font": "professor-minty",
+    "anger": 0,
+    "fear": 85,
+    "joy": 0,
+    "sadness": 10,
+    "analytical": 15,
+    "confident": 0,
+    "tentative": 35
+  }
+]
